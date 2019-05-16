0354f6777cd11479c0778be49f46308975e007b1
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-16 16:52:54 -0700

    created chars comp

---
+++ src/components/Characters.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+
+function Characters({ characters }) {
+  const charLis = characters.map(char => {
+    const { name, status, species, image } = char;
+    const charProps = { name, status, species, image };
+    return <Character key={JSON.stringify({ ...charProps })} {...charProps} />;
+  });
+
+  return (
+    <ul>
+      {charLis}
+    </ul>
+  );
+}
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+      species: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired
+    })
+  )
+};
+
+export default Characters;
