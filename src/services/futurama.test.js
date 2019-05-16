4ad57b35fef1ede95b8d4e81a523ef1630b77db2
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-16 14:47:58 -0700

    refactored a bit

--- src/services/futurama.test.js
+++ src/services/futurama.test.js
@@ -5,4 +5,8 @@ describe('futurama api tests', () => {
     const result = normalizeCharacter('Professor-farnsworth');
     expect(result).toBe('Professor Farnsworth');
   });
+  it('spits back the name if it is not split by a -', () => {
+    const result = normalizeCharacter('Bender');
+    expect(result).toBe('Bender');
+  });
 });
--- src/services/futuramaApi.js
+++ src/services/futuramaApi.js
@@ -11,10 +11,9 @@ export const getQuotes = (count = 5) => {
 
 export const normalizeCharacter = (character) => {
   if(character === character.split('-')) return character;
-  const charArr = character.split('-');
   let result = '';
-  charArr.forEach((word, i) => {
-    if(i < charArr.length - 1) {
+  character.split('-').forEach((word, i, splitArr) => {
+    if(i < splitArr.length - 1) {
       result += word[0].toUpperCase() + word.slice(1) + ' ';
     } else {
       result += word[0].toUpperCase() + word.slice(1);
