85774a5c777fe7639721db7de32f1c029269863f
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-16 13:55:09 -0700

    It is (a)live!

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import TopQuotes from '../containers/TopQuotes';
 
 export default function App() {
   return (
-    <p>Hello, It&apos;s Me</p>
+    <TopQuotes />
   );
 }
