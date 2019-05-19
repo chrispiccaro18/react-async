cbeb4cd853cfaceeea2f2d85169aa5e336d8cfde
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-19 14:52:58 -0700

    testing with dummy data

--- src/containers/AllCharacters.js
+++ src/containers/AllCharacters.js
@@ -1,20 +1,36 @@
 import React, { PureComponent } from 'react';
 import Characters from '../components/Characters';
 import { getCharacters } from '../services/rickAndMortyApi';
+import { characters, totalPages } from '../../data/characters';
 
 export default class AllCharacters extends PureComponent {
   state = {
     characters: [],
+    totalPages: null,
     page: 1
   }
 
   componentDidMount() {
-    getCharacters(this.state.page)
-      .then(characters => this.setState({ characters }));
+    this.setState({
+      characters,
+      totalPages
+    });
   }
 
   render() {
     const { characters } = this.state;
     return <Characters characters={characters} />;
   }
+}
+
+// eslint-disable-next-line no-unused-vars
+function forLater() {
+  getCharacters(this.state.page)
+    .then(res => {
+      const { characters, totalPages } = res;
+      return this.setState({
+        characters,
+        totalPages
+      });
+    });
 }
--- src/services/rickAndMortyApi.js
+++ src/services/rickAndMortyApi.js
@@ -1,13 +1,13 @@
 const BASE_URL = 'https://rickandmortyapi.com/api';
 
 export function getCharacters(page = 1) {
-  return fetch(`${BASE_URL}/characters/?page=${page}`)
+  return fetch(`${BASE_URL}/character/?page=${page}`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, body]) => {
       if(!ok) throw 'Unable to fetch characters';
       return {
         characters: body.results,
-        totalPages: body.info.count / body.info.pages
+        totalPages: Math.ceil(body.info.count / body.info.pages)
       };
     });
 }
